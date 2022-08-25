a1b96ea9a722901ba1f487c52e1cb8e0470d15e1
josephErian <gozefatef1999@gmail.com>
2022-08-25 14:58:50 +0200

    all done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Card from './Card'
+import data from "./data"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const cards = data.map(item => {
+    return (
+       <Card 
+          {...item}
+        />
+    )
+  })
+  return(
+    <div>
+      <header>
+        <img src='/Fill 213.png' />
+        <span>My Travel Journal</span>
       </header>
+      {cards}
     </div>
-  );
+  )
+  
 }
 
 export default App;
